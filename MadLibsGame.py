991cc200e1292b1b0fa5c3f6ba621c9957a5aaf0
NibirChowdhury <36859458+NibirChowdhury@users.noreply.github.com>
2022-11-27 18:23:54 +0600

    Add files via upload

---
+++ MadLibsGame.py
@@ -0,0 +1,10 @@
+print("Today I went to the zoo. I saw a ___________(adjective)  _____________(Noun) jumping up and down in its tree.  \nHe _____________(verb, past tense) __________(adverb)  through the large tunnel that led to its _______(adjective)  __________(noun). \nI got some peanuts and passed  them through the cage to a gigantic gray _______(noun)  towering above my head. \nFeeding that animal made  me hungry.\n")
+adjective = input("Enter an adjective: ")
+noun = input("Enter a noun: ")
+pastTenseVerb =  input("Enter a past tense verb: ")
+adverb = input("Enter an adverb: ")
+noun = input("Enter a noun: ")
+noun = input("Enter a noun: ")
+
+
+print("Today I went to the zoo. I saw a " + adjective +" " + noun + " jumping up and down in its tree. \nHe " + pastTenseVerb +" "+ adverb +" through the large tunnel that led to its " + adjective+ " "+  noun + ". \nI got some peanuts and passed  them through the cage to a gigantic gray "+ noun + " towering above my head. \nFeeding that animal made  me hungry.")
